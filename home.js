7f466d350e38b9968b923816de27a55b01156765
JensVA <jens.vanhove99@gmail.com>
2021-04-16 11:28:23 +0200

    changes made

--- home.js
+++ home.js
@@ -1,18 +1,40 @@
-console.log('Jens')
-//alert('yo');
+var randomimage = 2;
 
-document.getElementById("someText").innerHTML = 'Hey There';
+function backgroundBeige(){
+  document.getElementById("body").style.backgroundColor = "rgb(" + 247 + "," + 247 + "," + 242 + ")";
+}
 
-//var age = prompt('What is your age?');
-//document.getElementById('someText').innerHTML = age;
+function backgroundRed(){
+  document.getElementById("body").style.backgroundColor = "red";
+}
 
-var background = 0;
-function myFunction(){
-  if(background%2==0){
-    document.getElementById("body").style.background = "red";
-  }
-  else{
-    document.getElementById("body").style.background = "green";
-  }
-  background++;
+function backgroundGreen(){
+  document.getElementById("body").style.backgroundColor = "green";
+}
+
+function backgroundBlue(){
+  document.getElementById("body").style.backgroundColor = "lightskyblue";
+}
+
+function backgroundSelected(){
+  document.getElementById("body").style.backgroundColor = document.getElementById("input_color").value;
+}
+
+function backgroundManual(){
+  document.getElementById("body").style.backgroundColor = "rgb(" + document.getElementById("input_R").value + "," + document.getElementById("input_G").value + "," + document.getElementById("input_B").value + ")";
+}
+
+function randomImage(){
+  var width = document.getElementById("width").value;
+  var height = document.getElementById("height").value;
+  if(width==0)width = 300;
+  if(height==0)height = 200;
+
+  document.getElementById("randomimage_pic").src= "https://picsum.photos/" + width + "/" + height +"?random=" + randomimage;
+  randomimage++;
+}
+
+function resetImage(){
+  document.getElementById("randomimage_pic").src= "https://picsum.photos/300/200?random=" + randomimage;
+  randomimage++;
 }
